5146ab861ee385a0d0771a485df71c1db9c43a6e
Muaeen <muaeen.gharib@hotmail.com>
2024-01-09 11:34:57 +0400

    logging

--- 01_pandas.py
+++ 01_pandas.py
@@ -1,24 +1,39 @@
 import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.INFO, filename='py_log.log', filemode='w')
+s = '****************************************************************'
 
 # Creating a DataFrame without explicit index
 data_frame = pd.DataFrame({'first': [50,21], 'sec': [131, 2]})
-print(data_frame)
-print('****************************************************************')
+logging.info(data_frame)
+logging.info(s)
 
 # Here, a DataFrame is created with an explicit index, providing labels for the rows
 data_frame = pd.DataFrame({'first': [50,21], 'sec': [131, 2]},
                  index = ['product_1', 'product_2'])
-print(data_frame)
-print('****************************************************************')
+logging.info(data_frame)
+logging.info(s)
 
 # Creating a simple Series, which is a one-dimensional array-like object
 series = pd.Series([1,2,3,4,5])
-print(series)
-print('****************************************************************')
+logging.info(series)
+logging.info(s)
 
 # Creating a Series with an explicit index and a name
 series = pd.Series([1,2,3,4,5], 
                    index= ['first', 'second', 'third', 'fourth', 'fifth'],
                    name= 'Product_A')
-print(series)
-print('****************************************************************')
+logging.info(series)
+logging.info(s)
+
+
+df = pd.read_csv('winemag-data-130k-v2.csv')
+logging.info(df.head())
+
+print(df.shape)
+
+df = pd.read_csv('winemag-data-130k-v2.csv', index_col=0)
+logging.info(df)
+logging.info(s)
+
