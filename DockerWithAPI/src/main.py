4df30a7c14b9140b0d814ca402da272bc55fcd9e
Muaeen <muaeen.gharib@hotmail.com>
2024-01-24 13:26:52 +0400

    change the port and host

--- DockerWithAPI/src/main.py
+++ DockerWithAPI/src/main.py
@@ -8,4 +8,4 @@ def centarl_function():
     return {"Name": "Muaeen"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=7000, host="127.0.0.1")
+    uvicorn.run(app, port=2000, host="0.0.0.0")
