f9d72f805738d3c1a8b1d55f9ccd87a1c50b60a6
Muaeen <muaeen.gharib@hotmail.com>
2024-01-25 15:18:14 +0400

    modify

--- DockerWithAPI/run.sh
+++ DockerWithAPI/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd neural-api
+
 pip install -r requirements.txt
 
-python ./src/main.py --host  "0.0.0.0"  --port  "4000"  --reload
+python /src/main.py --host  "0.0.0.0"  --port  "4000"  --reload
--- DockerWithAPI/src/main.py
+++ DockerWithAPI/src/main.py
@@ -5,8 +5,10 @@ app = FastAPI()
 
 @app.get("/")
 def centarl_function():
-    return {"Name": "jhudsc",
-            "Age" : 0}
+    return {"Name": "aas",
+            "Age" : 535450}
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, port=4000, host="0.0.0.0")
