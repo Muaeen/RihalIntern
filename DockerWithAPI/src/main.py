764a6242c7d89f1f7a3ffd29b9ba5e66f490c3f9
Muaeen <muaeen.gharib@hotmail.com>
2024-01-29 15:50:32 +0400

    update

--- DockerWithAPI/src/main.py
+++ DockerWithAPI/src/main.py
@@ -8,7 +8,8 @@ def centarl_function():
     return {"Name": "Muaeen",
             "Age" : 535450}
 
+print(centarl_function())
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, port=4000, host="0.0.0.0")
+# if __name__ == "__main__":
+#     uvicorn.run(app, port=4000, host="0.0.0.0")
